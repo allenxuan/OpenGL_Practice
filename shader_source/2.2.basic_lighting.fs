64c5c5c7d1554f97e9f1da0fd2d44f51cc628ca7
allenxuan <allenxuan@gmail.com>
2019-12-23 22:48:01 +0800

    add demo basic_lighting_specular.cpp

--- shader_source/2.2.basic_lighting.fs
+++ shader_source/2.2.basic_lighting.fs
@@ -5,6 +5,7 @@ in vec3 Normal;
 in vec3 FragPos;
 
 uniform vec3 lightPos;
+uniform vec3 viewPos;
 uniform vec3 lightColor;
 uniform vec3 objectColor;
 
@@ -20,7 +21,13 @@ void main()
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
 
-    //diff may be 1, which can result in R, G, B value being greater than 1 ??
-    vec3 result = (ambient + diffuse) * objectColor;
+    // specular
+    float specularStrength = 0.5;
+    vec3 viewDir = normalize(viewPos - FragPos);
+    vec3 reflectDir = reflect(-lightDir, norm);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), 32);
+    vec3 specular = specularStrength * spec * lightColor;
+
+    vec3 result = (ambient + diffuse + specular) * objectColor;
     FragColor = vec4(result, 1.0);
 }
